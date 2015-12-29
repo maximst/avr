1d6b7ea51cf9a1f7e89adde167fd9d97b859d4c3
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2015-12-29 15:40:22 +0200

    Add synchro pin and define simple write functions

--- lib/write.c
+++ lib/write.c
@@ -1,11 +1,15 @@
 /* Library for writing ports data */
+#include <util/delay.h>
 #include "write.h"
 
 
-void serial_write(volatile uint8_t* port, const int pin, char data) {
+void serial_write(volatile uint8_t* port, const int pin, char data, const int sync_pin) {
     /* This function write serial data to defined port */
     int i;
-    char mask = 1 << pin;
+    char mask = (1 << pin);
+    char sync_mask = (1 << sync_pin);
+
+    *port &= ~sync_mask; // Set sync pin to "0"
 
     for (i = 8; i--; i >= 0) {
         if (i - pin >= 0) {
@@ -13,6 +17,14 @@ void serial_write(volatile uint8_t* port, const int pin, char data) {
         }
         else {
             *port &= ~mask | mask & data << pin - i;
+        }
+
+        // If sync_pin is set then him toggled
+        if (0 <= sync_pin <= 7) {
+            *port ^= sync_mask;
+            _delay_ms(1);
+            *port ^= sync_mask;
+            _delay_ms(1);
         }
     }
 }
--- lib/write.h
+++ lib/write.h
@@ -1,6 +1,12 @@
 #ifndef WRITE_H
 #define WRITE_H
- 
-void serial_write(volatile uint8_t* port, const int port_num, char data);
+#define set_h(port, pin) (port |= (1 << pin))
+#define set_l(port, pin) (port &= ~(1 << pin))
+#define set_h_pins(port, mask) (port |= mask)
+#define set_l_pins(port, mask) (port &= ~mask)
+#define inverse_pin(port, pin) (port ^= 1 << pin)
+#define inverse_pins(port, mask) (port ^= mask)
+
+void serial_write(volatile uint8_t* port, const int pin, char data, const int sync_pin);
  
 #endif
